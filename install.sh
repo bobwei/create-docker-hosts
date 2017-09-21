42f616dd9d2e8882c0eff453e8c54f7bd08e8b5c
Bob Wei <bob61016@gmail.com>
2017-09-22 01:23:45 +0800

    add comments

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# 1. get docker installation script and run
+# 2. start docker daemon
+# 3. add current user to "docker" group to run docker command without sudo
+# 4. login as current user again so that step 3 apply immediately
+
 curl -fsSL get.docker.com | sh && \
   sudo systemctl start docker && \
   sudo /usr/sbin/usermod -aG docker $USER && \
