6d15a1bd481a5e2620a1851a0e0043c88cb0af9d
Bob Wei <bob61016@gmail.com>
2017-09-22 11:06:17 +0800

    add daemon config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # 4. login as current user again so that step 3 apply immediately
 
 curl -fsSL get.docker.com | sh && \
+  sudo mkdir -p /etc/docker && sudo cp daemon.json /etc/docker/daemon.json && \
   sudo systemctl start docker && \
   sudo /usr/sbin/usermod -aG docker $USER && \
   sudo su - $USER
