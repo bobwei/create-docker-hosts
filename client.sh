0fc195ec4ac883878a38b10fc8c4b8aaf366a580
Bob Wei <bob61016@gmail.com>
2017-10-13 17:00:10 +0800

    rm unused

--- client.sh
+++
@@ -1,6 +0,0 @@
-docker                    \
-    --tls                \
-    --tlscacert=./certs/ca.pem         \
-    --tlscert=./certs/cert.pem         \
-    --tlskey=./certs/key.pem           \
-    -H=0.0.0.0:2376 ps
--- server.sh
+++
@@ -1,6 +0,0 @@
-sudo dockerd                 \
-    --tls                \
-    --tlscacert=./certs/ca.pem         \
-    --tlscert=./certs/server-cert.pem  \
-    --tlskey=./certs/server-key.pem    \
-    -H=0.0.0.0:2376
