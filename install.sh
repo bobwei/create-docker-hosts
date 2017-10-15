07e5f89f82165dd2a1716d646378203100e76323
Bob Wei <bob61016@gmail.com>
2017-10-15 22:49:38 +0800

    create certificates on vm

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 # 3. add current user to "docker" group to run docker command without sudo
 # 4. login as current user again so that step 3 apply immediately
 
+# print executed commands
+set -x
+
 curl -fsSL get.docker.com | sh && \
   sudo mkdir -p /etc/docker && sudo cp daemon.json /etc/docker/daemon.json && \
+  sudo ./create-certificates.sh && sudo mkdir -p /var/docker && sudo cp -r ./certs /var/docker && \
   sudo systemctl start docker && \
   sudo /usr/sbin/usermod -aG docker $USER && \
   sudo su - $USER
